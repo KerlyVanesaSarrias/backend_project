ddebe618443810bb4390206d08806eb2e7a9d559
Kerly Sarrias <kerlysarrias011@gmail.com>
2024-11-11 18:19:25 -0500

    validation for owner user stablishment

--- src/services/reservation/reservation.service.interface.ts
+++ src/services/reservation/reservation.service.interface.ts
@@ -2,7 +2,7 @@ import { Types } from 'mongoose';
 import { Reservation } from '../../interfaces/reservation.interface';
 
 export interface IReservationService {
-    getReservationsList(): Promise<Reservation[]>
+    getReservationsByUser(userId: string): Promise<Reservation[]>
     deleteById(reservationId: string): Promise<Reservation | null>;
     createReservation(reservation: Reservation, userId: string): Promise<Reservation | null>
 }
